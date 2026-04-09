a3ee2a4ba2a39cde7063a0b2f7841b04d9783ca2
javier.labradormunoz@alum.uca.es <91214410+lamux04@users.noreply.github.com>
2026-04-09 17:56:44 +0200

    .

---
+++ 04_experimentos/jobs/slurm/run_pd_CIC18__NearMiss_SMOTE_ENN__v1__validation_epochs_v1__5_5.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --job-name=pd_CIC18__NearMiss_SMOTE_ENN__v1__validation_epochs_v1__5_5
+#SBATCH --cpus-per-task=64
+#SBATCH --partition=normal
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=200GB
+#SBATCH --output=/home/inginf/u32902122/TFG/04_experimentos/logs/out/output_pd_CIC18__NearMiss_SMOTE_ENN__v1__validation_epochs_v1__5_5.log
+#SBATCH --mail-user=javier.labradormunoz@alum.uca.es
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT_80
+
+NOMBRE_SCRIPT="pd_CIC18__NearMiss_SMOTE_ENN__v1__validation_epochs_v1__5_5"
+PATH_SCRIPT=03_codigo/CIC18/05_validation_epochs/$NOMBRE_SCRIPT
+
+BASE=/home/inginf/u32902122/TFG/$PATH_SCRIPT
+NOTEBOOK=$BASE.ipynb
+NOTEBOOK_OUT="/home/inginf/u32902122/TFG/04_experimentos/logs/notebooks/${NOMBRE_SCRIPT}_executed_${SLURM_JOB_ID}.ipynb"
+
+CSV_NVIDIA="/home/inginf/u32902122/TFG/04_experimentos/logs/nvidia/${NOMBRE_SCRIPT}_${SLURM_JOB_ID}_nvidia-smi.csv"
+TXT_NVIDIA="/home/inginf/u32902122/TFG/04_experimentos/logs/nvidia/${NOMBRE_SCRIPT}_${SLURM_JOB_ID}_nvidia-smi.txt"
+
+module load Anaconda3/2024.02-1
+source activate tfgClean
+
+#------- Comando -------
+nvidia-smi > $TXT_NVIDIA
+cd $SLURM_SUBMIT_DIR
+
+nvidia-smi -lms 1000 --query-gpu=timestamp,pstate,power.management,power.draw,power.limit,power.default_limit,power.min_limit,power.max_limit,temperature.gpu,temperature.memory,memory.used,memory.total,memory.free,clocks.current.sm,clocks.current.memory --format=csv,nounits -f "$CSV_NVIDIA" &
+
+conda activate tfgClean
+export PYTHONNOUSERSITE=1   
+
+# Comando que realiza el trabajo:
+jupyter nbconvert \
+  --execute \
+  --to notebook \
+  --ExecutePreprocessor.kernel_name=tfgClean \
+  --ExecutePreprocessor.timeout=86400 \
+  --output "${NOTEBOOK_OUT}" \
+  "${NOTEBOOK}"
---
+++ 04_experimentos/jobs/slurm/run_pd_CIC18__RUS_SMOTE_ENN__v1__training_v1__2_5.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=pd_CIC18__RUS_SMOTE_ENN__v1__training_v1__2_5
+#SBATCH --cpus-per-task=64
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=200GB
+#SBATCH --output=/home/inginf/u32902122/TFG/04_experimentos/logs/out/output_pd_CIC18__RUS_SMOTE_ENN__v1__training_v1__2_5.log
+#SBATCH --mail-user=javier.labradormunoz@alum.uca.es
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT_80
+
+NOMBRE_SCRIPT="pd_CIC18__RUS_SMOTE_ENN__v1__training_v1__2_5"
+PATH_SCRIPT=03_codigo/CIC18/04_training/$NOMBRE_SCRIPT
+
+BASE=/home/inginf/u32902122/TFG/$PATH_SCRIPT
+NOTEBOOK=$BASE.ipynb
+NOTEBOOK_OUT="/home/inginf/u32902122/TFG/04_experimentos/logs/notebooks/${NOMBRE_SCRIPT}_executed_${SLURM_JOB_ID}.ipynb"
+
+CSV_NVIDIA="/home/inginf/u32902122/TFG/04_experimentos/logs/nvidia/${NOMBRE_SCRIPT}_${SLURM_JOB_ID}_nvidia-smi.csv"
+TXT_NVIDIA="/home/inginf/u32902122/TFG/04_experimentos/logs/nvidia/${NOMBRE_SCRIPT}_${SLURM_JOB_ID}_nvidia-smi.txt"
+
+module load Anaconda3/2024.02-1
+source activate tfgClean
+
+#------- Comando -------
+nvidia-smi > $TXT_NVIDIA
+cd $SLURM_SUBMIT_DIR
+
+nvidia-smi -lms 1000 --query-gpu=timestamp,pstate,power.management,power.draw,power.limit,power.default_limit,power.min_limit,power.max_limit,temperature.gpu,temperature.memory,memory.used,memory.total,memory.free,clocks.current.sm,clocks.current.memory --format=csv,nounits -f "$CSV_NVIDIA" &
+
+conda activate tfgClean
+export PYTHONNOUSERSITE=1   
+
+# Comando que realiza el trabajo:
+jupyter nbconvert \
+  --execute \
+  --to notebook \
+  --ExecutePreprocessor.kernel_name=tfgClean \
+  --ExecutePreprocessor.timeout=86400 \
+  --output "${NOTEBOOK_OUT}" \
+  "${NOTEBOOK}"
