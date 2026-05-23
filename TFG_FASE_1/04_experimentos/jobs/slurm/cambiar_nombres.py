e0363844df4c09d44a18018f3fdce56806b459d8
Javier Labrador <javilabra250604@gmail.com>
2026-05-23 18:13:41 +0200

    .

--- TFG_FASE_1/04_experimentos/jobs/slurm/cambiar_nombres.py
+++ TFG_FASE_1/04_experimentos/jobs/slurm/cambiar_nombres.py
@@ -3,7 +3,7 @@ import shutil
 
 if __name__ == '__main__':
     path_script = Path(__file__).parent
-    files_list = list(path_script.rglob('03_*/*.ipynb'))
+    files_list = list(path_script.rglob('*CIC17*.sh'))
     new_files_list = [Path(str(file.resolve()).replace('CIC17', 'BCCC17')) for file in files_list]
     for fs, fd in zip(files_list, new_files_list):
         fd.parent.mkdir(parents=True, exist_ok=True)
